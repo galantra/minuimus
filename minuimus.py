b0bc1ba4ff1d100a2322115f82cb2dc71ea97f10
galantra <52420923+galantra@users.noreply.github.com>
2023-06-26 16:40:44 +0000

    feat/progress bar, pause

--- minuimus.py
+++ minuimus.py
@@ -1,41 +1,73 @@
-
-
 import os
 import subprocess
+import pickle
 from multiprocessing import Pool
 from tqdm import tqdm
 
 # Define an array to store the folder paths
 folders = [
-    r"C:\Users\emanresu\Pictures\Interrail Juni 2016",
-    r"C:\Users\emanresu\Pictures\Johannes A5",
-    r"C:\Users\emanresu\Pictures\Abi 2016",
-    r"C:\Users\emanresu\Pictures\Pictures OnePlus 6",
-    r"C:\Users\emanresu\Pictures\Mallorca 2019",
-    r"C:\Users\emanresu\Pictures\London 2018",
-    r"C:\Users\emanresu\Pictures\Selbst",
-    r"C:\Users\emanresu\Pictures\Malta 2017"
+    # r"C:\Users\emanresu\AppData\Roaming\Anki2\Jura",
+    r"C:\Users\emanresu\Documents\Calibre Libraries",
+    # r"C:\Users\emanresu\AppData\Roaming\Anki2\Alg",
+    # r"C:\Users\emanresu\AppData\Roaming\Anki2\ES",
+    # r"C:\Users\emanresu\AppData\Roaming\Anki2\EN"
 ]
 
-def process_file(file_path):
+def process_file(file):
     """
     Process a file using the 'minuimus.pl' script.
     
     Args:
-        file_path (str): The path to the file to be processed.
+        file (str): The path to the file to be processed.
     """
-    subprocess.run(["perl", "minuimus.pl", "--zip-7z", file_path], check=True)
+    try:
+        subprocess.run(["perl", r"C:\Users\emanresu\PortableApps\minuimus\minuimus.pl", file], check=True)
+    except subprocess.CalledProcessError as e:
+        print(f"Error processing file: {file}. {e}")
+        # Handle the error here, if needed
+
+def update_progress_bar(progress, total, bar_length=40):
+    percent = float(progress) / total
+    filled_length = int(bar_length * percent)
+    bar = '=' * filled_length + '-' * (bar_length - filled_length)
+    return f"[{bar}] {int(percent * 100)}%"
 
 if __name__ == '__main__':
     files = []
     for folder in folders:
         for root, dirs, files_list in os.walk(folder):
             for filename in files_list:
-                if filename.endswith(".jpg") or filename.endswith(".png"):
-                    files.append(os.path.join(root, filename))
+                if not filename.endswith(".mp3"):
+                    file_path = os.path.join(root, filename)
+                    files.append(file_path)
 
     TOTAL_FILES = len(files)
-    with Pool(4) as p:
-        with tqdm(total=TOTAL_FILES, desc="Processing Files") as pbar:
-            for _ in p.imap_unordered(process_file, files):
-                pbar.update(1)
+    with Pool(12) as p:
+        with tqdm(total=TOTAL_FILES, desc="Processing Files", ncols=60) as pbar:
+            for i, _ in enumerate(p.imap_unordered(process_file, files), 1):
+                pbar.set_postfix({"File": f"{i}/{TOTAL_FILES}"})
+                pbar.set_description(update_progress_bar(i, TOTAL_FILES))
+                pbar.update()
+
+                # Check if the user wants to pause
+                if os.path.exists("pause.pickle"):
+                    p.close()  # Pause the Pool
+                    p.join()
+                    print("Script paused. Press 'Enter' to resume.")
+                    input()
+                    os.remove("pause.pickle")  # Remove the pause file
+                    p = Pool(12)  # Create a new Pool
+                    pbar = tqdm(total=TOTAL_FILES, desc="Processing Files", ncols=60, initial=i)
+                    pbar.set_description(update_progress_bar(i, TOTAL_FILES))
+                    pbar.update(i)
+
+                    # Save the progress
+                    progress = {
+                        "files": files,
+                        "total_files": TOTAL_FILES,
+                        "current_file_index": i
+                    }
+                    with open("progress.pickle", "wb") as f:
+                        pickle.dump(progress, f)
+
+    print("Processing complete!")
