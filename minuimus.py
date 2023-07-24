64315656d19ba02e5d8f67753eea342088f7db0b
galantra <52420923+galantra@users.noreply.github.com>
2023-07-24 18:39:20 +0200

    summary: add variable unit and percentage

--- minuimus.py
+++ minuimus.py
@@ -111,4 +111,18 @@ if __name__ == "__main__":
 
     # Calculate the total saved space
     total_saved_space = total_original_size - total_compressed_size
-    print(f"\nProcessing complete! Total saved space: {total_saved_space} bytes.")
+    percent_saved_space = (total_saved_space / total_original_size) * 100
+
+    if total_saved_space < 1024:
+        saved_space_unit = "bytes"
+    elif total_saved_space < 1048576:
+        saved_space_unit = "KB"
+        total_saved_space /= 1024
+    elif total_saved_space < 1073741824:
+        saved_space_unit = "MB"
+        total_saved_space /= 1048576
+    else:
+        saved_space_unit = "GB"
+        total_saved_space /= 1073741824
+
+    print(f"\nProcessing complete! Total saved space: {total_saved_space:.2f} {saved_space_unit} ({percent_saved_space:.2f}%).")
