fd68d7026edb3c992725f36052af93679ef65066
galantra <52420923+galantra@users.noreply.github.com>
2023-07-24 21:51:59 +0200

    various improvements

--- minuimus.py
+++ minuimus.py
@@ -1,37 +1,38 @@
+import os
 import sys
 import subprocess
 import logging
-from concurrent.futures import ProcessPoolExecutor, as_completed
-from tqdm import tqdm
-from pathlib import Path
 import multiprocessing
+from concurrent.futures import ThreadPoolExecutor, as_completed
+from tqdm import tqdm
+from humanize import naturalsize
 
 logging.basicConfig(level=logging.INFO)
 
-folder_path = Path(sys.argv[1])
-files = list(folder_path.glob('**/*.*'))
+folder_path = sys.argv[1]
+files = [entry.path for entry in os.scandir(folder_path) if entry.is_file()]
 total_original_size = 0
 total_compressed_size = 0
 
 def process_file(file):
     try:
-        original_size = file.stat().st_size
+        original_size = os.path.getsize(file)
         subprocess.run(
-            ["perl", "minuimus.pl", str(file)],
+            ["perl", "minuimus.pl", file],
             check=True,
-            creationflags=subprocess.BELOW_NORMAL_PRIORITY_CLASS,
+            # creationflags=subprocess.BELOW_NORMAL_PRIORITY_CLASS,
         )
-        compressed_size = file.stat().st_size
+        compressed_size = os.path.getsize(file)
         global total_original_size, total_compressed_size
         total_original_size += original_size
         total_compressed_size += compressed_size
     except subprocess.CalledProcessError as e:
-        logging.error(f"Error processing file: {file}. {e}")
+        logging.exception(f"Error processing file: {file}. {e}")
     except Exception as e:
-        logging.error(f"Error processing file: {file}. {e}")
+        logging.exception(f"Error processing file: {file}. {e}")
 
 if __name__ == '__main__':
-    with ProcessPoolExecutor(max_workers=multiprocessing.cpu_count()) as executor:
+    with ThreadPoolExecutor(max_workers=multiprocessing.cpu_count()) as executor:
         futures = [executor.submit(process_file, file) for file in files]
         for future in tqdm(as_completed(futures), total=len(futures), desc="Processing files"):
             pass
@@ -41,17 +42,7 @@ if __name__ == '__main__':
         percent_saved_space = 0
     else:
         percent_saved_space = (total_saved_space / total_original_size) * 100
-    if total_saved_space < 1024:
-        saved_space_unit = "bytes"
-    elif total_saved_space < 1048576:
-        saved_space_unit = "KB"
-        total_saved_space /= 1024
-    elif total_saved_space < 1073741824:
-        saved_space_unit = "MB"
-        total_saved_space /= 1048576
-    else:
-        saved_space_unit = "GB"
-        total_saved_space /= 1073741824
+    saved_space_str = naturalsize(total_saved_space)
     logging.info(
-        f"\nProcessing complete! Total saved space: {total_saved_space:.2f} {saved_space_unit} ({percent_saved_space:.2f}%)."
+        f"\nProcessing complete! Total saved space: {saved_space_str} ({percent_saved_space:.2f}%)."
     )
