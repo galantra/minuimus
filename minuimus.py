e9cf9c5b8eace2ad8e8b312be3ad8c5b750d59c5
galantra <52420923+galantra@users.noreply.github.com>
2023-07-24 17:29:22 +0200

    format

--- minuimus.py
+++ minuimus.py
@@ -19,38 +19,50 @@ for root, dirs, filenames in os.walk(folder_path):
     for filename in filenames:
         files.append(os.path.join(root, filename))
 
+
 # Define a function to process a single file
 def process_file(file):
     """
     Process a file using the 'minuimus.pl' script with below normal priority.
-    
+
     Args:
         file (str): The path to the file to be processed.
     """
     try:
         # Create the startupinfo object to modify the process creation flags
         startupinfo = subprocess.STARTUPINFO()
-        startupinfo.dwFlags |= subprocess.STARTF_USESHOWWINDOW  # Hide the console window
-        startupinfo.dwFlags |= subprocess.CREATE_NEW_CONSOLE  # Create a new console window for each subprocess
+        startupinfo.dwFlags |= (
+            subprocess.STARTF_USESHOWWINDOW
+        )  # Hide the console window
+        startupinfo.dwFlags |= (
+            subprocess.CREATE_NEW_CONSOLE
+        )  # Create a new console window for each subprocess
 
         # Launch the subprocess with below normal priority
-        subprocess.run(["perl", "minuimus.pl", file],
-                       check=True, startupinfo=startupinfo, creationflags=subprocess.BELOW_NORMAL_PRIORITY_CLASS)
+        subprocess.run(
+            ["perl", "minuimus.pl", file],
+            check=True,
+            startupinfo=startupinfo,
+            creationflags=subprocess.BELOW_NORMAL_PRIORITY_CLASS,
+        )
     except subprocess.CalledProcessError as e:
         print(f"Error processing file: {file}. {e}")
         # Handle the error here, if needed
+
 
 # Process each file in the files list
 for file in tqdm(files, desc="Processing files"):
     process_file(file)
 
+
 def update_progress_bar(progress, total, bar_length=40):
     percent = float(progress) / total
     filled_length = int(bar_length * percent)
-    bar = '=' * filled_length + '-' * (bar_length - filled_length)
+    bar = "=" * filled_length + "-" * (bar_length - filled_length)
     return f"[{bar}] {int(percent * 100)}%"
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     files = []
     for folder in folders:
         for root, dirs, files_list in os.walk(folder):
@@ -78,7 +90,7 @@ if __name__ == '__main__':
             progress = {
                 "files": files,
                 "total_files": TOTAL_FILES,
-                "current_file_index": i
+                "current_file_index": i,
             }
             with open("progress.pickle", "wb") as f:
                 pickle.dump(progress, f)
