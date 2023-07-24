25f250bd11c2ebcc0b8210016bdd3fde39c08083
galantra <52420923+galantra@users.noreply.github.com>
2023-07-24 22:21:39 +0200

    formatting

--- minuimus.py
+++ minuimus.py
@@ -9,6 +9,7 @@ from humanize import naturalsize
 
 logging.basicConfig(level=logging.INFO)
 
+
 def process_file(file):
     try:
         original_size = os.path.getsize(file)
@@ -23,15 +24,14 @@ def process_file(file):
         total_compressed_size += compressed_size
 
         input_creation_time = os.stat(file).st_ctime
-
         output_file = f"{file}.min"
-
         os.utime(output_file, (input_creation_time, input_creation_time))
 
     except subprocess.CalledProcessError as e:
         logging.exception(f"Error processing file: {file}. {e}")
     except Exception as e:
         logging.exception(f"Error processing file: {file}. {e}")
+
 
 def display_summary(files, total_original_size, total_compressed_size):
     num_files = len(files)
@@ -47,9 +47,12 @@ def display_summary(files, total_original_size, total_compressed_size):
     print(f"Number of files compressed: {num_files}")
     print(f"Total original size: {original_size_str}")
     print(f"Total compressed size: {compressed_size_str}")
-    print(f"Total saved space: {saved_space_str} ({percent_saved_space:.2f}% compression ratio)")
+    print(
+        f"Total saved space: {saved_space_str} ({percent_saved_space:.2f}% compression ratio)"
+    )
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     files = []
     for arg in sys.argv[1:]:
         if os.path.isfile(arg):
@@ -68,7 +71,9 @@ if __name__ == '__main__':
     total_compressed_size = 0
     with ThreadPoolExecutor(max_workers=multiprocessing.cpu_count()) as executor:
         futures = [executor.submit(process_file, file) for file in files]
-        for future in tqdm(as_completed(futures), total=len(futures), desc="Processing files"):
+        for future in tqdm(
+            as_completed(futures), total=len(futures), desc="Processing files"
+        ):
             pass
 
     display_summary(files, total_original_size, total_compressed_size)
